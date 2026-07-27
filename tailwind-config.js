073adabf7a5082f7c7ffafe1f91d68d7eb5f9773
Divyanshu Bawankule <diu.52kule@gmail.com>
2026-07-27 18:05:37 +0530

    Update tailwind-config.js

--- tailwind-config.js
+++ tailwind-config.js
@@ -2,8 +2,8 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
-        "primary": "#20419A","primary-container": "#6c63ac","on-primary": "#ffffff",
-        "primary-fixed": "#e5deff","primary-fixed-dim": "#c8bfff",
+        "primary": "#20419A","primary-container": "#20419A","on-primary": "#ffffff",
+        "primary-fixed": "#20419A","primary-fixed-dim": "#20419A",
         "surface": "#fcf8ff","surface-container-lowest": "#ffffff",
         "surface-container-low": "#f7f2f9","surface-container": "#f1ecf3",
         "surface-container-high": "#ebe7ee","surface-container-highest": "#e5e1e8",
